e7f80b54d07babd6a5624f648adab6b956697483
dmitry-sharashkin <97dima.sh@mail.ru>
2021-08-30 21:13:39 +0300

    added css modules

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,15 @@
 import './App.css';
 import React from 'react'
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="header">
-        <div className="logo">
-          <h2 className="logo__txt">social invest</h2>
-        </div>
-      </header>
-      <nav className="nav">
-        <a href="#">Profile</a>
-        <a href="#">Messages</a>
-        <a href="#">News</a>
-        <a href="#">Something</a>
-        <a href="#">Settings</a>
-      </nav>
-      <main className="main">
-        <div>
-          <img src="https://thumbs.dreamstime.com/z/%D1%80%D1%83%D0%BA%D0%B0-%D0%B6%D0%B5%D0%BD%D1%89%D0%B8%D0%BD%D1%8B-%D0%B4%D0%B5%D1%80%D0%B6%D0%B8%D1%82-%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B5-%D0%B4%D0%B5%D0%BD%D1%8C%D0%B3%D0%B8-%D0%BD%D0%B0%D0%BB%D0%B8%D1%87%D0%BD%D1%8B%D0%B5-%D0%B1%D1%83%D0%BC%D0%B0%D0%B3%D0%B8-%D0%B1%D0%B0%D0%BD%D0%BA%D0%BD%D0%BE%D1%82%D1%8B-%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%BE%D0%B9-138344989.jpg"></img>
-        </div>
-        <div>
-          ava disc
-        </div>
-        <div>
-          New post
-        </div>
-        <div>
-          post 2
-        </div>
-        <div>
-          post 3
-        </div>
-      </main>
+      <Header />
+      <Sidebar />
+      <Profile />
     </div>
   );
 }
---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import './Header.css'
+
+const Header = () => {
+   return (
+      <header className="header">
+         <div className="logo">
+            <h2 className="logo__txt">social invest</h2>
+         </div>
+      </header>
+   )
+}
+export default Header
