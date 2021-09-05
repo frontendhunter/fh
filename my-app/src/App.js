a5d70115ec12073cb0a8251209f87f5c646b183c
dmitry-sharashkin <faustfbs@mail.ru>
2021-09-05 16:59:40 +0300

    modified navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,23 @@ import React from 'react'
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Profile from './components/Profile/Profile';
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = () => {
-  return (
-    <div className="AppWrapper">
-      <Header />
-      <Sidebar />
-      {/*<Profile />*/}
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="AppWrapper">
+                <Header/>
+                <Sidebar/>
+                <div className='contentWrapper'>
+                    <Route path="/dialogs" render={Dialogs} />
+                    <Route path ="/profile" render={Profile} />
+                </div>
+
+            </div>
+        </BrowserRouter>
+    );
 }
 
 
--- my-app/src/components/Sidebar/Sidebar.jsx
+++ my-app/src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,14 @@
 import React from "react"
 import s from './Sidebar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Sidebar = () => {
    return (
       <nav className={s.nav}>
-         <a href="#" className={s.item}>Profile</a>
-         <a href="#" className={s.item}>Messages</a>
-         <a href="#" className={s.item}>News</a>
-         <a href="#" className={s.item}>Something</a>
-         <a href="#" className={s.item}>Settings</a>
+         <NavLink to="/profile" className={s.item} activeClassName={s.active} >Profile</NavLink>
+         <NavLink to="/dialogs" className={s.item} activeClassName={s.active} >Dialogs</NavLink>
+         <NavLink to="/news" className={s.item} activeClassName={s.active} >News</NavLink>
+         <NavLink to="/settings" className={s.item} activeClassName={s.active} >Settings</NavLink>
       </nav>
    )
 }
