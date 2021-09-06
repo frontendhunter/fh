0c30e69f91992a64a4c937bc1066bfde71a3ac26
dmitry-sharashkin <faustfbs@mail.ru>
2021-09-06 21:15:24 +0300

    modify  dialogs list

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,22 +1,16 @@
 import React from "react"
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className={s.dialogs__wrap}>
             <div className={s.dialogs}>
-                <div className={s.dialog__item}>
-                    Erjan
-                </div>
-                <div className={s.dialog__item}>
-                    Chuhan
-                </div>
-                <div className={s.dialog__item+' '+s.active}>
-                    Dushnila
-                </div>
-                <div className={s.dialog__item}>
-                    uRina
-                </div>
+                <NavLink to="/dialogs/1" className={s.dialog__item}> Erjan</NavLink>
+                <NavLink to="/dialogs/2" className={s.dialog__item}>Chuhan</NavLink>
+                <NavLink to="/dialogs/3" className={s.dialog__item + ' ' + s.active}>Dushnila</NavLink>
+                <NavLink to="/dialogs/4" className={s.dialog__item}>uRina</NavLink>
+
             </div>
             <div className={s.messages}>
                 <div className={s.messages__item}>
