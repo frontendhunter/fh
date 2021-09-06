d8238339258180ed422becd5d9c3dc7a99e5d7b8
dmitry-sharashkin <faustfbs@mail.ru>
2021-09-06 20:27:32 +0300

    dialogs interface uppdate

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,35 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            dialogs
+        <div className={s.dialogs__wrap}>
+            <div className={s.dialogs}>
+                <div className={s.dialog__item}>
+                    Erjan
+                </div>
+                <div className={s.dialog__item}>
+                    Chuhan
+                </div>
+                <div className={s.dialog__item+' '+s.active}>
+                    Dushnila
+                </div>
+                <div className={s.dialog__item}>
+                    uRina
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.messages__item}>
+                    hey, bro) What you doing today?
+                </div>
+                <div className={s.messages__item}>
+                    nothing
+                </div>
+                <div className={s.messages__item}>
+                    mb wolk?
+                </div>
+                <div className={s.messages__item}>
+                    No!
+                </div>
+            </div>
         </div>
 
     )
