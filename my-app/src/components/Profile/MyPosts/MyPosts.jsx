95e610a28adcbc1e92aac47aff0f3fe855243482
dmitry-sharashkin <faustfbs@mail.ru>
2021-09-07 20:44:47 +0300

    style changes

--- my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,17 +3,19 @@ import s from './MyPosts.module.css'
 import Post from "./Post/Post"
 
 const MyPosts = () => {
-   return (
-      <main className={s.main}>
-         <h4> My posts:</h4>
-         <form>
-            <textarea></textarea>
-            <button>add post</button>
-         </form>
+    return (
+        <div className={s.MyPosts}>
+            <h3> My posts:</h3>
+            <form>
+                <div><textarea></textarea></div>
+                <div>
+                    <button>add post</button>
+                </div>
+            </form>
 
-         <Post message='I am hangry' likesCount='12' />
-         <Post name="pie" message="hy, it's my first post)" likesCount='34' />
-      </main >
-   )
+            <Post message='I am hangry' likesCount='12'/>
+            <Post name="pie" message="hy, it's my first post)" likesCount='34'/>
+        </div>
+    )
 }
 export default MyPosts
--- my-app/src/components/Profile/profileInfo/ProfilInfo.jsx
+++ my-app/src/components/Profile/profileInfo/ProfilInfo.jsx
@@ -7,7 +7,7 @@ const ProfilInfo = () => {
             <div className={s.img__wrap}>
                 <img src='https://c4.wallpaperflare.com/wallpaper/88/467/746/oxbow-bend-at-sunset-grand-teton-national-park-wyoming-wallpaper-preview.jpg' alt="image"/>
             </div>
-            <div>
+            <div className={s.profileInfo}>
                 ava disc
             </div>
         </div>
