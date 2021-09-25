da7f19de8a3a0ab03c032b21422a73c6d6a01935
dmitry-sharashkin <faustfbs@mail.ru>
2021-09-25 19:19:00 +0300

    54 componentDidMount

--- my-app/src/components/Users/Users.jsx
+++ my-app/src/components/Users/Users.jsx
@@ -2,24 +2,21 @@ import React from "react";
 import s from './Users.module.css'
 import * as axios from "axios";
 
-class Users extends React.Component{
-    constructor(props) {
-        super(props);
-            if (this.props.users.length===0) {
-                axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                    this.props.setUsers(response.data.items)
-                });
-            }
-
+class Users extends React.Component {
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
 
-    render(){
+    render() {
         return <div className={s.all}>
             {
                 this.props.users.map(u => <div key={u.id}>
                     <div className={s.userContainer}>
                         <div className={s.image_wrap}>
-                            <img src={u.photos.small === null? 'https://st2.depositphotos.com/1104517/11965/v/600/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg':u.photos.small}/>
+                            <img
+                                src={u.photos.small === null ? 'https://st2.depositphotos.com/1104517/11965/v/600/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg' : u.photos.small}/>
                             {u.followed ?
                                 <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button> :
                                 <button onClick={() => this.props.follow(u.id)}>Follow</button>}
@@ -37,4 +34,5 @@ class Users extends React.Component{
         </div>
     }
 }
+
 export default Users;
