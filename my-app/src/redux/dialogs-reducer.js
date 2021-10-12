d46170dbe025bd6b2a199cccc798f088fcf3d68a
dmitry-sharashkin <faustfbs@mail.ru>
2021-10-12 20:11:13 +0300

    85  hook, useEffect

--- my-app/src/redux/dialogs-reducer.js
+++ my-app/src/redux/dialogs-reducer.js
@@ -3,27 +3,27 @@ const ADD_MESSAGE = 'ADD-MESSAGE';
 
 let initialState = {
     dialogs: [
-        {id: 1, name: 'Erjan', img: 'https://memepedia.ru/wp-content/uploads/2019/03/erzhan-768x569.jpg'},
+        {id: 1, name: 'Yury', img: 'https://memepedia.ru/wp-content/uploads/2019/03/erzhan-768x569.jpg'},
         {
             id: 2,
-            name: 'BrushGirl',
+            name: 'another girl',
             img: 'https://ru-minecraft.ru/uploads/posts/2018-02/1519154980_mad-villagers.png'
         },
         {
             id: 3,
-            name: 'uRina',
+            name: 'Rina',
             img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTirEEXAcgdftxMTICIHgtrNo9qBZhlLuUVlg&usqp=CAU'
         },
         {
             id: 4,
-            name: 'Dushnila',
+            name: 'Dan',
             img: 'https://media.professionali.ru/processor/topics/size5/2017/03/21/brodude-ru-8-06-2015-f2ecjf7krnt7z.jpg'
         },
     ],
     messages: [
         {
             id: 1,
-            name: 'BrushGirl',
+            name: 'another girl',
             img: 'https://ru-minecraft.ru/uploads/posts/2018-02/1519154980_mad-villagers.png',
             message: 'hello'
         },
@@ -32,20 +32,20 @@ let initialState = {
             name: 'me',
             class: 'textAlign: right;',
             img: 'https://pbs.twimg.com/profile_images/1412450786542379011/IY6aPa50.png',
-            message: 'sosi jopu'
+            message: 'Hy'
         },
         {
             id: 3,
-            name: 'me',
+            name: 'another girl',
             class: 'textAlign: right; ',
-            img: 'https://pbs.twimg.com/profile_images/1412450786542379011/IY6aPa50.png',
-            message: 'pisia popa pisia popa pisia popa pisia popa pisia popa  '
+            img: 'https://ru-minecraft.ru/uploads/posts/2018-02/1519154980_mad-villagers.png',
+            message: '  what are you doing '
         },
         {
             id: 4,
-            name: 'BrushGirl',
-            img: 'https://ru-minecraft.ru/uploads/posts/2018-02/1519154980_mad-villagers.png',
-            message: 'popa pisia popa pisia popa pisia'
+            name: 'me',
+            img: 'https://pbs.twimg.com/profile_images/1412450786542379011/IY6aPa50.png',
+            message: 'i am programming now'
         }
     ],
 };
