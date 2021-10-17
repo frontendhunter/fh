8f26a664dffd98c1afa1b8030ebd69fb624b244a
dmitry-sharashkin <faustfbs@mail.ru>
2021-10-17 10:18:24 +0300

    91 - chrome extensions React  Developer Tools & Redux DevTools

--- my-app/src/redux/redux-store.js
+++ my-app/src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
@@ -18,7 +18,9 @@ const reducers = combineReducers({
         form: formReducer,
     }
 )
-
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+));
 
 export default store;
