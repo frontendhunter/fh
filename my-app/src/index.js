e17d709f03bf36363028439e9de0545528cb490d
dmitry-sharashkin <faustfbs@mail.ru>
2021-11-01 15:31:01 +0300

    100 Cours Passed!

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import SamuraiJSApp from "./App";
 
+//Samurai Way 1.0 passed!!))
 
     ReactDOM.render(
        <SamuraiJSApp/>, document.getElementById('root')
