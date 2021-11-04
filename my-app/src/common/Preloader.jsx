4f755d591354cf398eb1424032a73e4820ad438f
dmitry-sharashkin <faustfbs@mail.ru>
2021-11-04 11:01:39 +0300

    new preloader

--- my-app/src/common/Preloader.jsx
+++ my-app/src/common/Preloader.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import preloader from "../assets/images/Rolling-0.6s-200px.svg";
+import s from "./Preloader.module.css"
 
 const Preloader = () => {
-    return <img src={preloader}/>
+    // return <img src={preloader}/>
+    return <div className={s.loader}>
+        <div className={s.l_main}>
+            <div className={s.l_square}><span></span><span></span><span></span></div>
+            <div className={s.l_square}><span></span><span></span><span></span></div>
+            <div className={s.l_square}><span></span><span></span><span></span></div>
+            {/*<div className={s.l_square}><span></span><span></span><span></span></div>*/}
+        </div>
+    </div>
 }
 export default Preloader
